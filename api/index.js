247165fd92bd27c9d3393ca4e511e1fd2c5fd7be
Shu Sia Lukito <slukito@slukito-a01.vmware.com>
2020-04-21 21:15:42 -0500

    Initial commit

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const db = require('./queries');
-// const tweet = require('./twitter');
 const port = 8080;
 
 app.use(cors());
@@ -18,9 +17,6 @@ app.get('/', (request, response) => {
 app.get('/accounts', db.getAccounts);
 app.get('/accounts/:account_num', db.getAccountById);
 app.get('/transactions/:account_num', db.getTransactions);
-
-// Twitter
-// app.get('/tweets', tweet.getTweets);
 
 app.listen(port, () => {
    console.log(`App running on port ${port}`);
