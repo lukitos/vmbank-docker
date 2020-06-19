7007a3ac76c2fc41e5c2a00cf314235ec1a4fe98
Shu Sia Lukito <slukito@slukito-a01.vmware.com>
2020-06-19 14:59:19 -0500

    first commit

--- api/queries.js
+++ api/queries.js
@@ -1,38 +1,55 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-   user: '<your-user>',
-   host: 'localhost',
+   user: 'postgres',
+   host: 'postgres',
    database: 'vmbank',
-   password: '<your-pwd>',
+   password: 'password',
    port: 5432,
 });
 
 const getAccounts = (request, response) => {
-   pool.query('SELECT * FROM account', (error, results) => {
-      if (error) {
-         throw error;
-      };
-      response.status(200).json(results.rows);
-   });
+   console.log('in getAccounts');
+   
+   pool.connect((err, client, release) => {
+      if (err) {
+        return console.error('Error acquiring client', err.stack)
+      }
+      pool.query('SELECT * FROM account', (error, results) => {
+         if (error) {
+            // console.log(error);
+            // throw error;
+            return console.error('Error in getAccounts', error.stack);
+         };
+         response.status(200).json(results.rows);
+      });
+    });
 };
 
 const getAccountById = (request, response) => {
+   console.log('in getAccountsById');
    const account_num = parseInt(request.params.account_num);
 
    pool.query('SELECT * FROM account WHERE account_num = $1', [account_num], (error, results) => {
       if (error) {
-         throw error;
+         // console.log(error);
+         // throw error;
+         return console.error('Error acquiring client', error.stack)
       };
       response.status(200).json(results.rows);
    });
 };
 
 const getTransactions = (request, response) => {
-    const account_num = parseInt(request.params.account_num);
+   console.log('in getTransactions');
+   const account_num = parseInt(request.params.account_num);
+   console.log('account_num', account_num);
  
     pool.query('SELECT * FROM transaction WHERE account_num = $1', [account_num], (error, results) => {
+       console.log(results);
        if (error) {
-          throw error;
+         // console.log(error);
+         // throw error;
+         return console.error('Error acquiring client', error.stack)
        };
        response.status(200).json(results.rows);
     });
--- db/init.sql
+++ db/init.sql
@@ -1,22 +1,18 @@
-Scripts to create db, tables, and populate the data:
-
-CREATE DATABASE vmbank;
-
 CREATE TABLE account (
-    account_num      integer PRIMARY KEY,
-    account_type      varchar(20),
-    name      varchar(20),
+    account_num    integer PRIMARY KEY,
+    account_type   varchar(20),
+    name           varchar(20),
     opened_by      varchar(20),
-    balance      numeric(10, 2)
+    balance        numeric(10, 2)
 );
 
 CREATE TABLE transaction (
-    trans_id      integer PRIMARY KEY,
-    account_num      integer,
+    trans_id        integer PRIMARY KEY,
+    account_num     integer,
     trans_type      varchar(20),
     trans_date      date,
-    amount      numeric(10, 2),
-    description      varchar(50),
+    amount          numeric(10, 2),
+    description     varchar(50),
     FOREIGN KEY (account_num) REFERENCES account (account_num)
 );
 
@@ -29,5 +25,3 @@ INSERT INTO transaction (trans_id, account_num, trans_type, trans_date, amount, 
     (2, 1000001, 'Deposit', '2020-04-15', 20000.00, 'Direct Deposit'),
     (3, 1000001, 'Withdrawal', '2020-04-20', 5000.00, 'Miscellaneous'),
     (4, 1000002, 'Deposit', '2020-04-01', 500000.00, 'Initial Deposit');
-
-
